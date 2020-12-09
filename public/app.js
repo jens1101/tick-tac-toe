c2be3f2a15b6d50cba7856792d04d05942e94ede
Jens Tschirpig <jens1101@users.noreply.github.com>
2020-12-09 23:14:33 +0200

    Handled the case when a client disconnects

--- public/app.js
+++ public/app.js
@@ -2,4 +2,16 @@
 
 (function () {
   const socket = io();
+
+  socket.on("waitingInLobby", () => {
+    console.log("Waiting in Lobby...");
+  });
+
+  socket.on("gameStart", () => {
+    console.log("The game has begun!");
+  });
+
+  socket.on("gameAbort", () => {
+    console.log("Other player aborted this game");
+  });
 })();
--- server/server.js
+++ server/server.js
@@ -10,8 +10,8 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {});
 
-const gameRoomIds = [];
-const lobbyRoomIds = [];
+const gameRoomIds = new Set();
+const lobbyRoomIds = new Set();
 
 // Set static folder
 app.use(express.static(path.join(__dirname, "../", "public")));
@@ -21,27 +21,51 @@ server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 // Handle new socket connection requests from client
 io.on("connection", (socket) => {
+  const gameRoomId = initConnection(socket);
+
+  socket.on("disconnect", () => onSocketDisconnect(socket, gameRoomId));
+
+  // TODO: handle the case when a game is completed
+  // TODO: handle gameplay events
+});
+
+/**
+ * Initialisation setup when a new client joins. This finds an available game in the lobby or, if
+ * none are available, will add the player to the lobby.
+ * @param socket
+ * @return {string} The room ID of the game that the user is part of.
+ */
+function initConnection(socket) {
   // Join a game in the lobby, if available
-  if (lobbyRoomIds.length > 0) {
-    const roomId = lobbyRoomIds.pop();
-    gameRoomIds.push(roomId);
-    joinRoom(roomId, socket);
-    return;
+  if (lobbyRoomIds.size > 0) {
+    const roomId = [...lobbyRoomIds].pop();
+    lobbyRoomIds.delete(roomId);
+    gameRoomIds.add(roomId);
+    socket.join(roomId);
+
+    io.to(roomId).emit("gameStart");
+
+    return roomId;
   }
 
   // If there are no games that can be joined then wait in the lobby.
   const roomId = `game_room ${uuid()}`;
-  lobbyRoomIds.push(roomId);
-  joinRoom(roomId, socket);
-
-  // TODO: handle the case when a game is completed
-  // TODO: handle the case when a user disconnects
-  // TODO: handle gameplay events
-});
-
-function joinRoom(roomId, socket) {
+  lobbyRoomIds.add(roomId);
   socket.join(roomId);
+  socket.emit("waitingInLobby");
 
-  // TODO: if first person in room then emit wait
-  // TODO: if second person to join then start the game
+  return roomId;
+}
+
+/**
+ * Handles the case when a client disconnects from the server. This informs all other clients in
+ * the current game (if any) that the game is aborted and removes the room from the list of rooms.
+ * @param socket
+ * @param gameRoomId
+ */
+function onSocketDisconnect(socket, gameRoomId) {
+  socket.to(gameRoomId).emit("gameAbort");
+
+  lobbyRoomIds.delete(gameRoomId);
+  gameRoomIds.delete(gameRoomId);
 }
