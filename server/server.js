9074d43516b577d09faa40be34ea5cd0e679ad03
Jens Tschirpig <jens1101@users.noreply.github.com>
2020-12-09 21:22:26 +0200

    Added basic functionality for joining a game

--- server/server.js
+++ server/server.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const path = require("path");
 const http = require("http");
-const socket = require("socket.io");
+const socketIo = require("socket.io");
+const { v4: uuid } = require("uuid");
 
 const PORT = process.env.PORT;
 
 const app = express();
 const server = http.createServer(app);
-const io = socket(server, {});
+const io = socketIo(server, {});
+
+const gameRoomIds = [];
+const lobbyRoomIds = [];
 
 // Set static folder
 app.use(express.static(path.join(__dirname, "../", "public")));
@@ -17,5 +21,27 @@ server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 // Handle new socket connection requests from client
 io.on("connection", (socket) => {
-  console.log("New web socket connection!");
+  // Join a game in the lobby, if available
+  if (lobbyRoomIds.length > 0) {
+    const roomId = lobbyRoomIds.pop();
+    gameRoomIds.push(roomId);
+    joinRoom(roomId, socket);
+    return;
+  }
+
+  // If there are no games that can be joined then wait in the lobby.
+  const roomId = `game_room ${uuid()}`;
+  lobbyRoomIds.push(roomId);
+  joinRoom(roomId, socket);
+
+  // TODO: handle the case when a game is completed
+  // TODO: handle the case when a user disconnects
+  // TODO: handle gameplay events
 });
+
+function joinRoom(roomId, socket) {
+  socket.join(roomId);
+
+  // TODO: if first person in room then emit wait
+  // TODO: if second person to join then start the game
+}
